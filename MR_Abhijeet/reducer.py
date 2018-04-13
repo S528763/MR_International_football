4c7f1b68456dc9eb5d9ca5a02922e884da38363e
Abhijeet <s530670@nwmissouri.edu>
2018-04-13 10:08:19 -0500

    Added functionality to compute draw matches

--- MR_Abhijeet/reducer.py
+++ MR_Abhijeet/reducer.py
@@ -1,11 +1,14 @@
 #Reading the content of the file
 s = open("sort_OP.txt","r")
 r = open("reducer_OP.txt", "w")
+f = open("final_OP.csv", "w")
 
 #initialiastion
 thisKey = ""
+thisTotalMatches = 0
 thisCounter = 0
 thisHomeWin = 0
+thisHomeDraw = 0
 for line in s:
   data = line.strip().split(',')
   home_team,home_score,away_score = data
@@ -20,13 +23,21 @@ for line in s:
     thisCounter = 0
   
   # apply the aggregation function
-  if int(home_score) > int(away_score):
+  thisTotalMatches += 1
+  if int(home_score) == int(away_score):
+    thisHomeDraw += 1
+  elif int(home_score) > int(away_score):
     thisHomeWin += 1
     thisCounter += 1
 
 # output the final entry when done
 r.write(thisKey + '\t' + str(thisCounter)+'\n')
-print("Totl win as a Home team :" +  str(thisHomeWin))
+f.write("Matches,Win,Loss,Draw Win\n")
+f.write(str(thisTotalMatches)+","+str(thisHomeWin)+","+str(thisTotalMatches - (thisHomeWin + thisHomeDraw))+","+str(thisHomeDraw))
+print("Total Number of matches:" +  str(thisTotalMatches))
+print("Total win as a Home team :" +  str(thisHomeWin))
+print("Total draw as a Home team :" +  str(thisHomeDraw))
+print("Total loss as a Home team :" +  str(thisTotalMatches - (thisHomeWin + thisHomeDraw)))
 
 s.close()
 r.close()
